d7a07a00938f3be40e2cadfa31f0b7253def7fa7
Taran Mitchell <taranfmitchell@gmail.com>
2017-07-13 05:53:56 -0700

    Initial commit

---
+++ all_pitchers.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  8 17:06:57 2017
+
+@author: taran
+"""
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.linalg import inv
+ 
+# Assign the filename: file
+file = 'pitchSequence_2016.csv'
+ 
+# Read the file into a DataFrame: df
+df = pd.read_csv(file)
+
+state = df.loc[:,['startingBalls','startingStrikes','balls','strikes','hitlocation']]
+state['strikes'].value_counts()
+
+state['startState']=state['startingBalls'].astype(str)+'-'+state['startingStrikes'].astype(str)
+
+#state['IP']=np.where(state['hitLocation']>0,'True','False')
+state['IP']=['False' if x == 0 else 'True' for x in state['hitlocation']]
+
+#state['BB']=np.where(state['endBalls']==4,'True','False')
+state['BB']=['True' if x == 4 else 'False' for x in state['balls']]
+
+#state['K']=np.where(state['endStrikes']==3,'True','False')
+state['K']=['True' if x == 3 else 'False' for x in state['strikes']]
+
+# create function to determine the end state: endState
+def endState(row):
+    ''' function that deteremines the outcome of the pitch and creates new state = endState'''
+    if row['BB'] == 'True':
+        val = 'BB'
+    elif row['K'] == 'True':
+        val = 'K'
+    elif row['IP'] == 'True':
+        val = 'IP'
+    else:
+        val = str(row['balls'])+'-'+str(row['strikes'])
+    return val
+
+# Add endState to the state frame
+state['endState'] = state.apply(endState, axis=1)
+
+transition_df = state.loc[:,['startState','endState']]
+
+state_pivot = transition_df.pivot_table(index=['startState'], columns='endState', aggfunc=len, fill_value=0, margins=True, margins_name='Total')
+
+table2 = state_pivot.div(state_pivot.iloc[:,-1], axis=0 )
+
+clean = table2.iloc[:-1,:-1]
+
+extra_rows = pd.DataFrame({'0-1': [0,0,0],
+'0-2': [0,0,0],
+'1-0': [0,0,0],
+'1-1': [0,0,0],
+'1-2': [0,0,0],
+'2-0': [0,0,0],
+'2-1': [0,0,0],
+'2-2': [0,0,0],
+'3-0': [0,0,0],
+'3-1': [0,0,0],
+'3-2': [0,0,0],
+'BB': [1,0,0],
+'IP': [0,1,0],
+'K': [0,0,1]},
+index=['BB', 'IP', 'K'])
+
+result = clean.append(extra_rows)
+
+# BREAKS HERE: Wrong number of items passed 15, placement implies 21
+# due to 4-x and x-3 counts in result table
+# missing 0-0 as end state so that the matrix is 15x15 square
+result['0-0'] = pd.Series([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], index=result.index)
+
